354fbeab84479ec81f2c346c6a9b2caba8abf863
Anto-Olguin <olguinbantonella@gmail.com>
2023-09-30 18:08:06 -0300

    modificacion para modal

--- scripts.js
+++ scripts.js
@@ -1,35 +1,62 @@
-// Inicializa la puntuación en 0
-let puntaje = 0
-const opciones = ["piedra", "papel", "tijera"]
+// Inicializa puntuacion
+let puntaje = parseInt(localStorage.getItem("puntaje")) || 0;
+const opciones = ["piedra", "papel", "tijera"];
+const botones = document.querySelectorAll(".container button");
 
-function eleccionPc(){
+botones.forEach((button) => {
+    button.addEventListener("click", () => {
+        partida(button.id);
+    });
+});
+
+let medallas = [];
+
+fetch('./rewards.json')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    medallas = data.regalos;
+});
+
+function eleccionPc() {
     const randomIndex = Math.floor(Math.random() * opciones.length)
     return opciones[randomIndex]
 }
 
-function ronda(eleccionJugador, eleccionCompu){
-    if(eleccionJugador === eleccionCompu){
+function ronda(eleccionJugador, eleccionCompu) {
+    if (eleccionJugador === eleccionCompu) {
         return "Empate."
-    }else if(
+    } else if (
         (eleccionJugador === "piedra" && eleccionCompu === "tijera") ||
         (eleccionJugador === "papel" && eleccionCompu === "piedra") ||
         (eleccionJugador === "tijera" && eleccionCompu === "papel")
-    ){
-        // Suma 3 puntos al puntaje si ganas
-        puntaje += 3;
+    ) {
         return "¡Ganaste!"
-    }else{
+    } else {
         return "¡Perdiste!"
     }
 }
 
-function partida(eleccionJugador){
+function actualizarPuntaje(puntos) {
+    puntaje += puntos;
+    localStorage.setItem("puntaje", puntaje);
+
+    // Muestra el puntaje
+    const puntajeMostrado = document.querySelector(".puntaje");
+    puntajeMostrado.textContent = `Puntaje: ${puntaje}`;
+}
+
+function partida(eleccionJugador) {
     const eleccionCompu = eleccionPc()
     const resultado = ronda(eleccionJugador, eleccionCompu)
     const resultadoMostrado = document.querySelector(".resultado p")
     resultadoMostrado.textContent = `Elegiste ${eleccionJugador}. La computadora eligió ${eleccionCompu}. ${resultado}`
 
-    if(resultado === "¡Ganaste!"){
+    if (resultado === "¡Ganaste!") {
+
+        // Sumo 3 puntos
+        actualizarPuntaje(3)
+
         Toastify({
             text: "¡Sumaste 3 puntos!",
             className: "info",
@@ -38,21 +65,62 @@ function partida(eleccionJugador){
             },
         }).showToast()
     }
-
-// Guarda el puntaje en local
-localStorage.setItem("puntaje", puntaje)
 }
 
-// Recupera el puntaje almacenado en local
-puntaje = parseInt(localStorage.getItem("puntaje")) || 0
+actualizarPuntaje(0);
 
-// Muestra el puntaje
-const puntajeMostrado = document.querySelector(".puntaje")
-puntajeMostrado.textContent = `Puntaje: ${puntaje}`
+const modal = document.getElementById('shopModal');
+const abrirModalBtn = document.getElementById('shop');
+const cerrarModalBtn = document.getElementById('cerrarModal');
+const medallasLista = document.getElementById('medallasLista');
 
-const botones = document.querySelectorAll("button")
-botones.forEach((button) => {
-    button.addEventListener("click", () => {
-    partida(button.id);
+
+abrirModalBtn.addEventListener('click', abrirModal);
+cerrarModalBtn.addEventListener('click', cerrarModal);
+
+function abrirModal() {
+    modal.style.display = 'block';
+    cargarLista();
+}
+
+function cerrarModal() {
+modal.style.display = 'none';
+}
+
+function cargarLista() {
+    medallasLista.innerHTML = '';
+
+    medallas.forEach((item) => {
+        const divClassMedalla = document.createElement('div');
+        divClassMedalla.classList.add('medalla');
+
+        const imgMedalla = document.createElement('img');
+        imgMedalla.src = item.imagen;
+
+        const pMedalla = document.createElement('p');
+        pMedalla.innerText = `${item.descripcion} \n ${item.puntos} puntos`;
+
+        const btnComprar = document.createElement('button');
+        btnComprar.id = `medalla-${item.id}`;
+        btnComprar.classList.add('btn-comprar');
+        btnComprar.innerText = 'Comprar regalo';
+        btnComprar.addEventListener('click', () => comprarMedalla(item.id));
+
+        divClassMedalla.appendChild(imgMedalla);
+        divClassMedalla.appendChild(pMedalla);
+        divClassMedalla.appendChild(btnComprar);
+
+        medallasLista.appendChild(divClassMedalla);
     });
-});
+}
+
+function comprarMedalla(numeroDeMedalla) {
+    const medallaElementoHtml = document.getElementById(`medalla-${numeroDeMedalla}`);
+    let medalla = medallas.find(medalla => medalla.id == numeroDeMedalla);
+
+    if (puntaje >= medalla.puntos && !medalla.comprada) {
+        actualizarPuntaje(-medalla.puntos);
+        medallaElementoHtml.innerText = 'Regalo comprado!';
+        medalla.comprada = true;
+    }
+}
